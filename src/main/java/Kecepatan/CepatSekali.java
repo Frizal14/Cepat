b6193c055c01faef70994fd952d17a79b4667588
Ferdy Rizal Mahendra Putra <nakalrizal472@gmail.com>
2023-10-29 21:21:24 +0700

    menambahkan folder

--- src/main/java/Kecepatan/CepatSekali.java
+++ src/main/java/Kecepatan/CepatSekali.java
@@ -1,2 +1,30 @@
-package Kecepatan;public class CepatSekali {
+package Kecepatan;
+import java.util.Scanner;
+public class CepatSekali {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Program Konversi Suhu");
+        System.out.print("Pilih jenis konversi (1 untuk Fahrenheit ke Celsius, 2 untuk Celsius ke Fahrenheit): ");
+        int pilihan = input.nextInt();
+
+        if (pilihan == 1) {
+            // Konversi dari Fahrenheit ke Celsius
+            System.out.print("Masukkan suhu dalam Fahrenheit: ");
+            double fahrenheit = input.nextDouble();
+            double celsius = (fahrenheit - 32) * 5.0 / 9.0;
+            System.out.println("Suhu dalam Celsius: " + celsius + "°C");
+        } else if (pilihan == 2) {
+            // Konversi dari Celsius ke Fahrenheit
+            System.out.print("Masukkan suhu dalam Celsius: ");
+            double celsius = input.nextDouble();
+            double fahrenheit = (celsius * 9.0 / 5.0) + 32;
+            System.out.println("Suhu dalam Fahrenheit: " + fahrenheit + "°F");
+        } else {
+            System.out.println("Pilihan tidak valid. Harap pilih 1 atau 2.");
+        }
+
+        input.close();
+    }
 }
